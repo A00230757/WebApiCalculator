6fe4d5fd701a312bd98cd91ec709d737cd8f7a73
A00230757 <84326658+A00230757@users.noreply.github.com>
2021-10-31 06:45:33 -0400

    i changed name of class library from class1 to CalculatorTasks and made new function in it named AdditionTask which accept two numbers and return their sum. Code runs successfully and helpful comments are added.

---
+++ ClassLibraryCalculator/CalculatorTasks.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ClassLibraryCalculator
+{
+    public class CalculatorTasks
+    {
+        public static double AdditionTask(double number1, double number2)//this task of class library accept
+                                                                         //two numbers of double data type and return their sum
+        {
+            return number1 + number2;
+        }
+    }
+}
--- ClassLibraryCalculator/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace ClassLibraryCalculator
-{
-    public class Class1
-    {
-
-    }
-}
