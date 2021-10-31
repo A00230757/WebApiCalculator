b535218b52816b207c4c5ced19c638d7cd0f080a
A00230757 <84326658+A00230757@users.noreply.github.com>
2021-10-31 06:53:29 -0400

    I made new function SubtractionTask in Class library which  accept two numbers of double data type and check which number is greater and then subtract them and return the result.Code runs successfully and extra comments are added.

--- ClassLibraryCalculator/CalculatorTasks.cs
+++ ClassLibraryCalculator/CalculatorTasks.cs
@@ -9,5 +9,20 @@ namespace ClassLibraryCalculator
         {
             return number1 + number2;
         }
+        public static double SubtractionTask(double number1, double number2)//this task of class library accept
+                                                                         //two numbers of double data type and check which number is greater
+                                                                         //amd then subtract them and return the result
+        {
+            double answer=0;
+            if( number1>number2)
+            {
+               answer = number1-number2;
+            }
+            else
+            {
+                answer = number2-number1;
+            }
+            return answer;
+        }
     }
 }
