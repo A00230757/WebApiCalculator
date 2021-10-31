fd5e2d959a60af41dda0b7980ebdd10a64838e2b
A00230757 <84326658+A00230757@users.noreply.github.com>
2021-10-31 07:56:12 -0400

    In WebApiCalculator a new method is made which accept two numbers in double format and return result of  their sum using class library task addition already made. code properly executes, solves the challenge and contains explanatory comments

---
+++ WebApiCalculator/Controllers/CalculatorController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClassLibraryCalculator;
+
+namespace WebApiCalculator.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpGet]
+        public double AddTwoNumbers([FromQuery] double number1, [FromQuery] double  number2)
+            //this method of WebApi accept two numbers
+            //in double data type
+            //and use class library tasks already
+            //made to return their sum
+        {
+
+            return CalculatorTasks.AdditionTask(number1, number2);
+        }
+    }
+}
